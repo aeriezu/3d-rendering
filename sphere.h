69552375a553910a925834e6114759fee3e47357
Elese Marie <emzukina@gmail.com>
2025-09-10 11:18:00 -0400

    adding metal spheres to main and fixing some errors + new render

--- sphere.h
+++ sphere.h
@@ -5,7 +5,7 @@
 
 class sphere : public hittable {
     public:
-        sphere(const point3& center, double radius)
+        sphere(const point3& center, double radius, shared_ptr<material> mat)
         : center(center), radius(std::fmax(0,radius)), mat(mat) {}
 
         bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
